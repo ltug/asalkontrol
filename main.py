3d99265b8989d2ed68e6987f19d3040f2702e6cb
ltug <ltug26@gmail.com>
2021-05-07 21:26:38 +0300

    master

---
+++ main.py
@@ -0,0 +1,51 @@
+sayi1 =int(input("Sayı 1'i girin"))
+if sayi1 > 1:
+
+    for i in range(2,sayi1):
+        if (sayi1 % i) == 0:
+            print(sayi1,"asal sayı değildir")
+            break
+        else:
+            print(sayi1,"Asal sayıdır.")
+            break
+else:
+    print(sayi, " Asal Sayı Değildir.")
+sayi2 =int(input("Sayı 2'yı girin"))
+if sayi2 > 1:
+
+    for i in range(2,sayi2):
+        if(sayi2 % i)==0:
+            print(sayi2,("asal sayı değildir"))
+            break
+        else:
+            print(sayi2,("Asal sayıdır."))
+            break
+sayi3 =int(input("Sayı 3 'ü girin"))
+if sayi3 > 1:
+
+    for i in range(2,sayi3):
+        if(sayi3 % i)==0:
+            print(sayi3,("asal sayı değildir"))
+            break
+        else:
+            print(sayi3,("Asal sayıdır."))
+sayi4 =int(input("Sayı 4 ü girin"))
+if sayi4 > 1:
+
+    for i in range(2,sayi4):
+        if(sayi4 % i)==0:
+            print(sayi4,("asal sayı değildir"))
+            break
+        else:
+            print(sayi4,("Asal sayıdır."))
+sayi5  =int(input("Sayı 5 'i girin "))
+if sayi5 > 1:
+
+    for i in range(2,sayi4):
+        if(sayi5 % i)==0:
+            print(sayi5,("asal sayı değildir"))
+            break
+        else:
+            print(sayi5,("Asal sayıdır."))
+
+
